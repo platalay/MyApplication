b87ff0be54448d387b9329091ca1d82e467ac0c4
Piyachoke Sinanun <platalay@gmail.com>
2020-06-25 13:37:51 +0700

    Initial commit

---
+++ app/src/main/java/platalay/com/MyApplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package platalay.com.MyApplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
